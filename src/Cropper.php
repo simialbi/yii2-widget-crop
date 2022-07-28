03d3d554549417d0b299c04dc885daa8066bf50a
Simon Karlen <simi.albi@outlook.com>
2022-07-28 08:50:01 +0200

    bumped to php 7.3 compatibility with bs5

--- src/Cropper.php
+++ src/Cropper.php
@@ -44,233 +44,241 @@ use yii\web\JsExpression;
  * @author Simon Karlen <simi.albi@gmail.com>
  * @since 1.0
  */
-class Cropper extends Widget {
-	const TYPE_MODAL = 'modal';
-	const TYPE_INLINE = 'inline';
-	const TYPE_BUTTON = 'button';
-	/**
-	 * @var string URL for send crop data
-	 */
-	public $cropUrl;
-	/**
-	 * @var string Original image URL
-	 */
-	public $image;
-	/**
-	 * @var float Aspect ratio for crop box. If not set(null) - it means free aspect ratio
-	 */
-	public $aspectRatio;
-	/**
-	 * @var string display type of cropper
-	 */
-	public $type = self::TYPE_INLINE;
-	/**
-	 * @var string Modal class name. Only required if $type is TYPE_MODAL
-	 */
-	public $modalClass;
-	/**
-	 * @var array Modal view options
-	 */
-	public $modalOptions = [];
-	/**
-	 * @var array Button view options
-	 */
-	public $buttonOptions = [];
-	/**
-	 * @var array HTML widget options
-	 */
-	public $options = [];
-	/**
-	 * @var array HTML-options for image tag
-	 */
-	public $imageOptions = [];
-	/**
-	 * @var string Text to show in button
-	 */
-	public $buttonContent = 'Crop {icon}';
-	/**
-	 * @var string Icon to show in button
-	 */
-	public $buttonIcon = '<i class="glyphicon glyphicon-scissors"></i>';
-	/**
-	 * @var array Additional cropper options
-	 * @see https://github.com/fengyuanchen/cropper#options
-	 */
-	public $clientOptions = [];
-	/**
-	 * @var array the event handlers for the underlying plugin.
-	 * @see https://github.com/fengyuanchen/cropper#events
-	 */
-	public $clientEvents = [];
-	/**
-	 * @var array Ajax options for send crop-requests
-	 */
-	public $ajaxOptions = [];
-	/**
-	 * @var array Default HTML-options for image tag
-	 */
-	private $_defaultImageOptions = [
-		'class' => 'cropper-image img-responsive',
-		'alt'   => 'crop-image'
-	];
-	/**
-	 * @var array Default cropper options
-	 * @see https://github.com/fengyuanchen/cropper/blob/master/README.md#options
-	 */
-	private $_defaultClientOptions = [
-		'strict'           => true,
-		'autoCropArea'     => 1,
-		'checkImageOrigin' => false,
-		'checkCrossOrigin' => false,
-		'checkOrientation' => false,
-		'zoomable'         => false
-	];
+class Cropper extends Widget
+{
+    const TYPE_MODAL = 'modal';
+    const TYPE_INLINE = 'inline';
+    const TYPE_BUTTON = 'button';
+    /**
+     * @var string URL for send crop data
+     */
+    public $cropUrl;
+    /**
+     * @var string Original image URL
+     */
+    public $image;
+    /**
+     * @var float Aspect ratio for crop box. If not set(null) - it means free aspect ratio
+     */
+    public $aspectRatio;
+    /**
+     * @var string display type of cropper
+     */
+    public $type = self::TYPE_INLINE;
+    /**
+     * @var string Modal class name. Only required if $type is TYPE_MODAL
+     */
+    public $modalClass;
+    /**
+     * @var array Modal view options
+     */
+    public $modalOptions = [];
+    /**
+     * @var array Button view options
+     */
+    public $buttonOptions = [];
+    /**
+     * @var array HTML widget options
+     */
+    public $options = [];
+    /**
+     * @var array HTML-options for image tag
+     */
+    public $imageOptions = [];
+    /**
+     * @var string Text to show in button
+     */
+    public $buttonContent = 'Crop {icon}';
+    /**
+     * @var string Icon to show in button
+     */
+    public $buttonIcon = '<i class="glyphicon glyphicon-scissors"></i>';
+    /**
+     * @var array Additional cropper options
+     * @see https://github.com/fengyuanchen/cropper#options
+     */
+    public $clientOptions = [];
+    /**
+     * @var array the event handlers for the underlying plugin.
+     * @see https://github.com/fengyuanchen/cropper#events
+     */
+    public $clientEvents = [];
+    /**
+     * @var array Ajax options for send crop-requests
+     */
+    public $ajaxOptions = [];
+    /**
+     * @var array Default HTML-options for image tag
+     */
+    private $_defaultImageOptions = [
+        'class' => 'cropper-image img-responsive',
+        'alt' => 'crop-image'
+    ];
+    /**
+     * @var array Default cropper options
+     * @see https://github.com/fengyuanchen/cropper/blob/master/README.md#options
+     */
+    private $_defaultClientOptions = [
+        'strict' => true,
+        'autoCropArea' => 1,
+        'checkImageOrigin' => false,
+        'checkCrossOrigin' => false,
+        'checkOrientation' => false,
+        'zoomable' => false
+    ];
 
-	/**
-	 * {@inheritdoc}
-	 * @throws InvalidConfigException
-	 */
-	public function init() {
-		$this->registerTranslations();
+    /**
+     * {@inheritDoc}
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        $this->registerTranslations();
 
-		if (!isset($this->options['id'])) {
-			$this->options['id'] = $this->getId();
-		}
-		if (empty($this->modalOptions)) {
-			$this->modalOptions = [
-				'header' => Html::tag('h3', Yii::t('simialbi/crop/cropper', 'Select crop area and click "crop" button'), [
-					'class' => 'modal-title'
-				])
-			];
-		}
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        if (empty($this->modalOptions)) {
+            $this->modalOptions = [
+                'header' => Html::tag('h3', Yii::t('simialbi/crop/cropper', 'Select crop area and click "crop" button'), [
+                    'class' => 'modal-title'
+                ])
+            ];
+        }
 
-		if ($this->type === self::TYPE_MODAL) {
-			if (empty($this->modalClass)) {
-				$this->modalClass = 'yii\bootstrap\Modal';
-			}
-			if (!class_exists($this->modalClass)) {
-				$this->modalClass = 'yii\bootstrap4\Modal';
-			}
-			if (!class_exists($this->modalClass)) {
-				throw new InvalidConfigException("Either 'yiisoft/yii2-bootstrap' or 'yiisoft/yii2-bootstrap4' must be installed to use as TYPE_MODAL!");
-			}
-		}
+        if ($this->type === self::TYPE_MODAL) {
+            if (empty($this->modalClass)) {
+                $this->modalClass = 'yii\bootstrap\Modal';
+            }
+            if (!class_exists($this->modalClass)) {
+                $this->modalClass = 'yii\bootstrap4\Modal';
+            }
+            if (!class_exists($this->modalClass)) {
+                $this->modalClass = 'yii\bootstrap5\Modal';
+            }
+            if (!class_exists($this->modalClass)) {
+                throw new InvalidConfigException("Either 'yiisoft/yii2-bootstrap' or 'yiisoft/yii2-bootstrap4' or 'yiisoft/yii2-bootstrap5' must be installed to use as TYPE_MODAL!");
+            }
+        }
 
-		parent::init();
-	}
+        parent::init();
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function run() {
-		$imageOptions = ArrayHelper::merge($this->_defaultImageOptions, $this->imageOptions);
+    /**
+     * {@inheritDoc}
+     */
+    public function run(): string
+    {
+        $imageOptions = ArrayHelper::merge($this->_defaultImageOptions, $this->imageOptions);
 
-		ob_start();
+        ob_start();
 
-		switch ($this->type) {
-			case self::TYPE_MODAL:
-			case self::TYPE_BUTTON:
-				$buttonOptions = ArrayHelper::merge([
-					'class' => 'btn btn-primary'
-				], $this->buttonOptions, [
-					'id'   => '#' . $this->options['id'] . '-btn',
-					'data' => [
-						'toggle' => ($this->type === self::TYPE_MODAL) ? 'modal' : 'collapse',
-						'target' => '#' . $this->options['id'] . '-target'
-					]
-				]);
+        switch ($this->type) {
+            case self::TYPE_MODAL:
+            case self::TYPE_BUTTON:
+                $buttonOptions = ArrayHelper::merge([
+                    'class' => 'btn btn-primary'
+                ], $this->buttonOptions, [
+                    'id' => '#' . $this->options['id'] . '-btn',
+                    'data' => [
+                        'toggle' => ($this->type === self::TYPE_MODAL) ? 'modal' : 'collapse',
+                        'target' => '#' . $this->options['id'] . '-target'
+                    ]
+                ]);
 
-				if ($this->type === self::TYPE_BUTTON) {
-					$buttonOptions['aria-expanded'] = 'false';
-					$buttonOptions['aria-controls'] = $this->options['id'] . '-target';
-				}
+                if ($this->type === self::TYPE_BUTTON) {
+                    $buttonOptions['aria-expanded'] = 'false';
+                    $buttonOptions['aria-controls'] = $this->options['id'] . '-target';
+                }
 
-				echo Html::a(
-					Yii::t('simialbi/crop/cropper', $this->buttonContent, ['icon' => $this->buttonIcon]),
-					['#' . $this->options['id']],
-					$buttonOptions
-				);
+                echo Html::a(
+                    Yii::t('simialbi/crop/cropper', $this->buttonContent, ['icon' => $this->buttonIcon]),
+                    ['#' . $this->options['id']],
+                    $buttonOptions
+                );
 
-				if ($this->type === self::TYPE_MODAL) {
-					$footer = Html::button(Yii::t('simialbi/crop/cropper', 'Close'), [
-						'type'         => 'button',
-						'class'        => 'btn btn-default',
-						'data-dismiss' => 'modal'
-					]);
-					$footer .= Html::button(Yii::t('simialbi/crop/cropper', $this->buttonContent, ['icon' => $this->buttonIcon]), [
-						'type'         => 'button',
-						'class'        => 'btn btn-success',
-						'data-dismiss' => 'modal'
-					]);
+                if ($this->type === self::TYPE_MODAL) {
+                    $footer = Html::button(Yii::t('simialbi/crop/cropper', 'Close'), [
+                        'type' => 'button',
+                        'class' => 'btn btn-default',
+                        'data-dismiss' => 'modal'
+                    ]);
+                    $footer .= Html::button(Yii::t('simialbi/crop/cropper', $this->buttonContent, ['icon' => $this->buttonIcon]), [
+                        'type' => 'button',
+                        'class' => 'btn btn-success',
+                        'data-dismiss' => 'modal'
+                    ]);
 
-					$modalOptions           = $this->modalOptions;
-					$modalOptions['id']     = $this->options['id'] . '-target';
-					$modalOptions['footer'] = $footer;
+                    $modalOptions = $this->modalOptions;
+                    $modalOptions['id'] = $this->options['id'] . '-target';
+                    $modalOptions['footer'] = $footer;
 
-					call_user_func([$this->modalClass, 'begin'], $modalOptions);
-				} elseif ($this->type === self::TYPE_BUTTON) {
-					echo Html::beginTag('div', [
-						'id'    => $this->options['id'] . '-target',
-						'class' => 'collapse'
-					]);
-				}
+                    call_user_func([$this->modalClass, 'begin'], $modalOptions);
+                } elseif ($this->type === self::TYPE_BUTTON) {
+                    echo Html::beginTag('div', [
+                        'id' => $this->options['id'] . '-target',
+                        'class' => 'collapse'
+                    ]);
+                }
 
-			case self::TYPE_INLINE:
-				$options = $this->options;
-				Html::addCssClass($options, 'crop-image-container');
-				echo Html::beginTag('div', $options);
-				echo Html::img($this->image, $imageOptions);
-				echo Html::endTag('div');
+            case self::TYPE_INLINE:
+                $options = $this->options;
+                Html::addCssClass($options, 'crop-image-container');
+                echo Html::beginTag('div', $options);
+                echo Html::img($this->image, $imageOptions);
+                echo Html::endTag('div');
 
-				if ($this->type === self::TYPE_MODAL) {
-					call_user_func([$this->modalClass, 'end']);
-					break;
-				} elseif ($this->type === self::TYPE_BUTTON) {
-					echo Html::endTag('div');
-					break;
-				}
-		}
-		$content = ob_get_clean();
+                if ($this->type === self::TYPE_MODAL) {
+                    call_user_func([$this->modalClass, 'end']);
+                    break;
+                } elseif ($this->type === self::TYPE_BUTTON) {
+                    echo Html::endTag('div');
+                    break;
+                }
+        }
+        $content = ob_get_clean();
 
-		$this->registerPlugin();
+        $this->registerPlugin();
 
-		return $content;
-	}
+        return $content;
+    }
 
-	/**
-	 * Init translations
-	 */
-	protected function registerTranslations() {
-		Yii::$app->i18n->translations['simialbi/crop*'] = [
-			'class'          => 'yii\i18n\GettextMessageSource',
-			'sourceLanguage' => 'en-US',
-			'basePath'       => __DIR__ . '/messages'
-		];
-	}
+    /**
+     * Init translations
+     */
+    protected function registerTranslations()
+    {
+        Yii::$app->i18n->translations['simialbi/crop*'] = [
+            'class' => 'yii\i18n\GettextMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => __DIR__ . '/messages'
+        ];
+    }
 
-	/**
-	 * Registers the assets and builds the required js for the widget
-	 */
-	protected function registerPlugin() {
-		$id   = $this->options['id'];
-		$view = $this->getView();
+    /**
+     * Registers the assets and builds the required js for the widget
+     */
+    protected function registerPlugin()
+    {
+        $id = $this->options['id'];
+        $view = $this->getView();
 
-		CropperAsset::register($view);
+        CropperAsset::register($view);
 
-		$clientOptions = ArrayHelper::merge($this->_defaultClientOptions, $this->clientOptions);
-		$selector      = "#$id > img";
-		$js            = '';
+        $clientOptions = ArrayHelper::merge($this->_defaultClientOptions, $this->clientOptions);
+        $selector = "#$id > img";
+        $js = '';
 
-		if (!empty($this->aspectRatio)) {
-			$clientOptions['aspectRatio'] = $this->aspectRatio;
-		}
+        if (!empty($this->aspectRatio)) {
+            $clientOptions['aspectRatio'] = $this->aspectRatio;
+        }
 
-		$clientOptions = Json::encode($clientOptions);
-		$jsId          = Inflector::slug($id, '_');
+        $clientOptions = Json::encode($clientOptions);
+        $jsId = Inflector::slug($id, '_');
 
-		switch ($this->type) {
-			case self::TYPE_MODAL:
-				$js = <<<JS
+        switch ($this->type) {
+            case self::TYPE_MODAL:
+                $js = <<<JS
 var modal$jsId = jQuery('#$id-target');
 var image$jsId = jQuery('$selector');
 
@@ -283,48 +291,49 @@ modal$jsId.on({
 	}
 });
 JS;
-				break;
-			case self::TYPE_BUTTON:
-			case self::TYPE_INLINE:
-				$js = <<<JS
+                break;
+            case self::TYPE_BUTTON:
+            case self::TYPE_INLINE:
+                $js = <<<JS
 var image$jsId = jQuery('$selector');
 image$jsId.cropper($clientOptions);
 JS;
 
-				break;
-		}
+                break;
+        }
 
-		if (!empty($this->cropUrl)) {
-			$ajaxOptions = Json::encode(ArrayHelper::merge([
-				'url'      => Url::to($this->cropUrl),
-				'method'   => 'POST',
-				'data'     => new JsExpression("image.cropper('getData')"),
-				'dataType' => 'JSON'
-			], $this->ajaxOptions));
-			$js          .= <<<JS
+        if (!empty($this->cropUrl)) {
+            $ajaxOptions = Json::encode(ArrayHelper::merge([
+                'url' => Url::to($this->cropUrl),
+                'method' => 'POST',
+                'data' => new JsExpression("image.cropper('getData')"),
+                'dataType' => 'JSON'
+            ], $this->ajaxOptions));
+            $js .= <<<JS
 image$jsId.on('crop', function() {
 	jQuery.ajax($ajaxOptions);
 });
 JS;
-		}
+        }
 
-		$view->registerJs($js);
+        $view->registerJs($js);
 
 
-		$this->registerClientEvents();
-	}
+        $this->registerClientEvents();
+    }
 
-	/**
-	 * Registers JS event handlers that are listed in [[clientEvents]].
-	 */
-	protected function registerClientEvents() {
-		if (!empty($this->clientEvents)) {
-			$id = $this->options['id'];
-			$js = [];
-			foreach ($this->clientEvents as $event => $handler) {
-				$js[] = "jQuery('#$id').on('$event', $handler);";
-			}
-			$this->view->registerJs(implode("\n", $js));
-		}
-	}
+    /**
+     * Registers JS event handlers that are listed in [[clientEvents]].
+     */
+    protected function registerClientEvents()
+    {
+        if (!empty($this->clientEvents)) {
+            $id = $this->options['id'];
+            $js = [];
+            foreach ($this->clientEvents as $event => $handler) {
+                $js[] = "jQuery('#$id').on('$event', $handler);";
+            }
+            $this->view->registerJs(implode("\n", $js));
+        }
+    }
 }
--- src/CropperAsset.php
+++ src/CropperAsset.php
@@ -11,32 +11,34 @@ use yii\web\AssetBundle;
  * @author Simon Karlen <simi.albi@gmail.com>
  * @since 1.0
  */
-class CropperAsset extends AssetBundle {
-	public $sourcePath = '@bower';
-	public $css = [
-		'cropperjs/dist/cropper.min.css'
-	];
-	public $js = [
-		'cropperjs/dist/cropper.min.js',
+class CropperAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+    public $css = [
+        'cropperjs/dist/cropper.min.css'
+    ];
+    public $js = [
+        'cropperjs/dist/cropper.min.js',
         'jquery-cropper/dist/jquery-cropper.min.js'
-	];
-	public $depends = [
-		'yii\web\JqueryAsset'
-	];
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function init() {
-		if (YII_DEBUG) {
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        if (YII_DEBUG) {
             foreach ($this->js as $k => $js) {
                 $this->js[$k] = str_replace('.min', '', $js);
             }
             foreach ($this->css as $k => $css) {
                 $this->css[$k] = str_replace('.min', '', $css);
             }
-		}
+        }
 
-		parent::init();
-	}
+        parent::init();
+    }
 }
